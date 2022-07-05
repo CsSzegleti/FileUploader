9a720935e43306d86ca8bcb3c97fbb385b0ce2a2
Csongor Szegleti <szegleti_csongor@4dsoft.hu>
2022-07-05 09:15:54 +0200

    List all allowed file extensions

---
+++ src/main/java/com/fileuploader/r34dy/service/AllowedExtensions.java
@@ -0,0 +1,16 @@
+package com.fileuploader.r34dy.service;
+
+public enum AllowedExtensions {
+    DOC,
+    DOCX,
+    HTML,
+    HTM,
+    PDF,
+    ODT,
+    XLS,
+    XLSX,
+    ODS,
+    PPT,
+    PPTX,
+    TXT
+}
